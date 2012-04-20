41911caea9fdc13aed4aff5b4459aa0b6c164d2f
rgrant222 <rgrant222@gmail.com>
2012-04-19 21:06:54 -0400

    Catch up

--- trgs/bob/views.py
+++ trgs/bob/views.py
@@ -1,11 +1,14 @@
 # Create your views here.
 # Create your views here.
 
-from django.template import Context, RequestContext, loader
+import django.template
 from django.shortcuts import render_to_response
-from django.http import HttpResponse
+import django.http
 
 def bob(request):
+    """
+
+    """
     return render_to_response('bob.html',
             {},
-        context_instance=RequestContext(request))
+        context_instance=django.template.RequestContext(request))
