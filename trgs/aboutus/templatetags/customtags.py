2a7c5a35aba0c033ce9eab271d2e622f89bc8c68
rgrant222 <rgrant222@gmail.com>
2012-04-19 22:25:53 -0400

    Added some custom template tags that allow indexing into lists and mappings.

---
+++ trgs/aboutus/templatetags/customtags.py
@@ -0,0 +1,19 @@
+__author__ = 'rob'
+
+from django import template
+
+register = template.Library()
+
+@register.filter(name="lookup")
+def lookup(d, k, f=""):
+  try:
+    return d.get(k, f)
+  except Exception:
+    return ""
+
+@register.filter(name="lookup")
+def index(d, i):
+  try:
+    return d[i]
+  except Exception:
+    return ""
