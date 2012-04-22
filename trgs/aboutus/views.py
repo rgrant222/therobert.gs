779fc9a88e332b506572f41ab4f3220a679a6522
rob <rgrant222@gmail.com>
2012-04-21 23:39:29 -0400

    Removed an unused data view

--- trgs/aboutus/views.py
+++ trgs/aboutus/views.py
@@ -1,8 +1,7 @@
 # Create your views here.
-from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
-import json
+
 
 def default(request):
   people = ['Rob', 'Bob']
@@ -46,23 +45,3 @@ def default(request):
 
   return render_to_response('aboutus.html', c,
                             context_instance=RequestContext(request))
-
-
-def data(request):
-  rob_profile = {
-    'name': 'Rob',
-    'age': 28,
-    'city': 'Dacula',
-    'state': 'Georgia',
-    'picture': 'rob-profile.png',
-  }
-
-  data = []
-  for n in range(10):
-    data.append({
-      'question': 'Title of Section %d' % n,
-      'content': 'Here is the content of %d' % n,
-    })
-  
-  return HttpResponse(json.dumps({'sections': data}, indent=2, default=str),
-                      content_type="application/json") 
