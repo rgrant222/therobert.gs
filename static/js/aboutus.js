cb6cbfe7dee30a3beac6f982def32bd9005f7b64
rob <rgrant222@gmail.com>
2012-04-22 00:38:13 -0400

    Removed the old test version and began implementing the new version.

--- static/js/aboutus.js
+++ static/js/aboutus.js
@@ -1,31 +1,15 @@
-var data = undefined;
-var d3HookId = '#d3_start';
+var d3HookId = '#d3';
 
 $(document).ready(function () {
-  d3.json('/aboutus/data/', buildPage);
+  buildDiagram(questions)
 });
 
 
-var buildPage = function (responseData) {
-  console.log("Displaying error, aha!");
-  data = responseData;
-  
-  var sections = d3.select(d3HookId).selectAll('div.section')
-      .data(responseData.sections);
-  
-  sections.enter().append('div')
-      .classed('section', true)
-      .classed('row', true)
-    .append('div')
-      .classed('span12', true)
-      .classed('question', true)
-    .append('div')
-      .classed('title', true);
+var buildDiagram = function (data) {
+  var svg = d3.select(d3HookId).append('svg')
+		  .attr('width', 940)
+		  .attr('heght', 470)
+		  .attr('transform', 'translate(-0.5, -0.5)');
 
-  var titles = sections.selectAll('.title');
-  titles.append('h1')
-      .text(function (d) { return d.question; });
-  titles.append('p')
-      .text(function (d) { return d.content; });
 };
 
