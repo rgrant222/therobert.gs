1066445d3e9527f0eea1c2914fc7e3a0da7a575d
rob <rgrant222@gmail.com>
2012-04-21 23:38:22 -0400

    Added a basic view that loads the ideas template

--- trgs/ideas/views.py
+++ trgs/ideas/views.py
@@ -1,1 +1,8 @@
 # Create your views here.
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
+
+
+def default(request):
+	c = {}
+	return render_to_response('ideas.html', c, context_instance=RequestContext(request))
