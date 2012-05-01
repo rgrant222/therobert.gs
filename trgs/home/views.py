f913a16837e31e4f1fdb5d27f82057709472812a
rob <rgrant222@gmail.com>
2012-04-30 21:38:45 -0400

    Put the posts into the template context

--- trgs/home/views.py
+++ trgs/home/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponse
 
 def home(request):
-    return render_to_response('home.html', 
-                              {}, 
+	c = {}
+	c['happening_posts'] = pymongo.Connection()['trgs']['posts'].find().sort([('date', -1)]).limit(5)
+	return render_to_response('home.html',
+                              c,
                               context_instance=RequestContext(request))
