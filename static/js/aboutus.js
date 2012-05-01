7d8fda1b7119482f12d6574ab405754690a15636
rob <rgrant222@gmail.com>
2012-04-30 20:01:50 -0400

    Fixed the redundancy and stupidity, and added a little silliness.

--- static/js/aboutus.js
+++ static/js/aboutus.js
@@ -1,14 +1,13 @@
 var d3HookId = '#d3';
+// var questions = [it's cool, it's been put here by the django template system!]
 
-$(document).ready(function () {
-  buildDiagram(questions)
-});
+$(document).ready(buildDiagram);
 
 
-var buildDiagram = function (data) {
-  var svg = d3.select(d3HookId).append('svg')
+var buildDiagram = function () {
+	var svg = d3.select(d3HookId).append('svg')
 		  .attr('width', 940)
-		  .attr('heght', 470)
+		  .attr('heght', 320)
 		  .attr('transform', 'translate(-0.5, -0.5)');
 
 };
